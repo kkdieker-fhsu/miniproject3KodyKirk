d73bc9055307f9446213000c018b8a524136e529
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-10-26 14:40:23 -0800

    Beginning file transfer functionality

--- tabletoplocal/pages.py
+++ tabletoplocal/pages.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -8,10 +10,12 @@ from tabletoplocal.db import get_db
 
 bp = Blueprint('pages', __name__)
 
+#the landing page
 @bp.route('/')
 def landing():
     return render_template('pages/landing.html')
 
+#the chat page and associated function to get chat messages from database
 @bp.route('/chat', methods=('GET', 'POST'))
 @login_required
 def chat():
@@ -24,6 +28,7 @@ def chat():
     ).fetchall()
     return render_template('pages/chat.html', chats=chats)
 
+#add new chat message to the database
 @bp.route('/create', methods=('GET','POST'))
 @login_required
 def create():
@@ -45,29 +50,29 @@ def create():
 
     return redirect(url_for('pages.chat'))
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
+#the files page and its functions
 @bp.route('/files')
 @login_required
 def files():
-    return render_template('pages/files.html')
+    if not os.path.exists('uploads'):
+        os.mkdir('uploads')
+    upload_path = 'uploads'
+    filetree = filetree(upload_path)
 
+    return render_template('pages/files.html', filetree=filetree)
+
+def filetree(fullpath):
+    tree = []
+    for root, dirs, files in os.walk(fullpath):
+        for name in sorted(files):
+            path = os.path.join(root, name)
+            tree.append(path)
+    return tree
+
+#the games page and associated functions
 @bp.route('/games', methods=('GET','POST'))
 @login_required
+#add new game data to the database
 def games():
     if request.method == 'POST':
         title = request.form['title']
@@ -90,19 +95,23 @@ def games():
 
     return render_template('pages/games.html', games=get_games(), role=g.user['role'], servers=active_servers())
 
+#get the number of games in the database
 def get_games():
     games = get_db().execute(
         'SELECT COUNT(*) FROM tables'
     ).fetchone()[0]
     return games
 
+#get data on the active servers
 def active_servers():
     servers = get_db().execute(
         'SELECT * FROM tables'
     ).fetchall()
     return servers
 
+#load the resources page
 @bp.route('/resources')
 @login_required
 def resources():
     return render_template('pages/resources.html')
+
