9e06f1cfdbe7122a112be06eeb5459eaa6bbdda8
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-10-26 06:36:37 -0800

    Testing flask

--- flaskexp.py
+++ flaskexp.py
@@ -4,6 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+    return '<h1>Home page</h1>'
+@app.route('/hello')
 def hello_world():
     return '<p>Hello World!</p>'
